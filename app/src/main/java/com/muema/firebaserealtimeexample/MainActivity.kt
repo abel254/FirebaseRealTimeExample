36f429bf6c625cf6cb96d68dff941cb119515877
abel254 <abelmuema353@gmail.com>
2026-02-03 21:09:02 +0300

    adding data to firebaseRealTimeDb

--- app/src/main/java/com/muema/firebaserealtimeexample/MainActivity.kt
+++ app/src/main/java/com/muema/firebaserealtimeexample/MainActivity.kt
@@ -6,14 +6,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.muema.firebaserealtimeexample.databinding.ActivityMainBinding
 
  class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding 
-    private lateinit var firebaseRef : DatabaseReference
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +23,11 @@ import com.muema.firebaserealtimeexample.databinding.ActivityMainBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseRef = FirebaseDatabase.getInstance().getReference("test")
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.addFragment))
 
-        binding.txtSendData.setOnClickListener {
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-            firebaseRef.setValue("Abel Kotlin Developer")
-                .addOnCompleteListener {
-                    Toast.makeText(this, "data stored successfully", Toast.LENGTH_SHORT).show()
-                }
-        }
 
 
 
---
+++ app/src/main/java/com/muema/firebaserealtimeexample/models/Contacts.kt
@@ -0,0 +1,7 @@
+package com.muema.firebaserealtimeexample.models
+
+data class Contacts(
+    val id: String? = null,
+    val name: String? = null,
+    val phoneNumber: String? = null
+)
