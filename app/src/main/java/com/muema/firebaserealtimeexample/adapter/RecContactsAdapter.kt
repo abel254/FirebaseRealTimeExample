dda01fe7ebe7d16443b9e7e028ea36cda455bc74
abel254 <abelmuema353@gmail.com>
2026-02-04 23:12:14 +0300

    deleting data from firebaseRealTimeDb

--- app/src/main/java/com/muema/firebaserealtimeexample/adapter/RecContactsAdapter.kt
+++ app/src/main/java/com/muema/firebaserealtimeexample/adapter/RecContactsAdapter.kt
@@ -3,8 +3,11 @@ package com.muema.firebaserealtimeexample.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.database.FirebaseDatabase
 import com.muema.firebaserealtimeexample.HomeFragmentDirections
 import com.muema.firebaserealtimeexample.databinding.RecContactsItemBinding
 import com.muema.firebaserealtimeexample.models.Contacts
@@ -38,6 +41,28 @@ class RecContactsAdapter(private val contactList: ArrayList<Contacts>) : Recycle
                         currentItem.phoneNumber.toString()
                     )
                     findNavController(holder.itemView).navigate(action)
+                }
+
+                recContainer.setOnLongClickListener {
+                    MaterialAlertDialogBuilder(holder.itemView.context)
+                        .setTitle("Delete item permanently")
+                        .setMessage("Are you sure you want to delete this item?")
+                        .setPositiveButton("Yes"){_,_ ->
+                            val firebaseRef = FirebaseDatabase.getInstance().getReference("contacts")
+                            firebaseRef.child(currentItem.id.toString()).removeValue()
+                                .addOnSuccessListener {
+                                    Toast.makeText(holder.itemView.context, "Item removed successfully", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(holder.itemView.context, "error: ${it.message}", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                        .setNegativeButton("No"){_,_ ->
+                            Toast.makeText(holder.itemView.context, "cancelled", Toast.LENGTH_SHORT).show()
+                        }
+                        .show()
+
+                    return@setOnLongClickListener true
                 }
             }
         }
